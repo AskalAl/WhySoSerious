682309971f967c03c1ebad00ae762f877d7bc5b6
AskalAl <39438977+AskalAl@users.noreply.github.com>
2020-11-25 14:37:32 +0200

    Add files via upload

---
+++ WIP/SAY/README.markdown
@@ -0,0 +1,8 @@
+# Web Speech API and Animation with Annyang
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/askalal/pen/eYdOKyv](https://codepen.io/askalal/pen/eYdOKyv).
+
+Having problems with 'end callback looping - causes "voice recognition closing" sound to keep generating every few seconds (Android Samsung S6)
+
+This has been acknowledged:
+https://github.com/TalAter/annyang/issues/194
---
+++ WIP/SAY/src/script.js
@@ -0,0 +1,84 @@
+// ------------------------------------
+// ------------------------------------
+
+$( document ).ready(function() {
+  $("h1").html("Say: View Glacier Park");
+  $('div.move').each(function(i) {
+    $(this).addClass('small-vibe' + i);
+  });
+  // $('div.move').each(function(i, obj) {
+  //   $(this).removeClass('small-vibe' + i);
+  //   $(this).addClass('big-vibe' + i);
+  // });
+  setTimeout(function() {
+    $("h1").toggleClass('slide-in');
+  }, 1000);
+});
+
+if (annyang) {
+  annyang.addCallback('result', function() {
+    console.log('soundstart');
+    $('div.move').each(function(i, obj) {
+      $(this).removeClass('big-vibe' + i);
+    });
+  });
+
+  annyang.addCallback('end', function() {
+    console.log('ended');
+    //restart();
+    $('div.move').each(function(i, obj) {
+      $(this).removeClass('big-vibe' + i);
+      $(this).addClass('small-vibe' + i);
+    });
+  });
+
+  annyang.addCallback('result', function() {
+    console.log('result');
+    //restart();
+    $('div.move').each(function(i, obj) {
+      $(this).removeClass('big-vibe' + i);
+      $(this).addClass('small-vibe' + i);
+    });  
+  });
+
+  var commands = {
+    '(view) glacier park': function() {
+      $.ajax({
+        type: 'GET',
+        dataType: 'jsonp',
+        url: 'https://peterdoes.it/gnp-tmp/gallery.json?=',
+        async: false,
+        jsonpCallback: 'jsonCallback',
+        contentType: "application/json",
+        beforeSend: function(){
+          $("h1").html("Getting thumbnails...");
+          $('.overlay, .loader').fadeIn('fast');
+        },
+        complete: function(){
+          $('.overlay, .loader').fadeOut('fast');
+          // setTimeout(function() {
+          //   $("h1").html("Say: 'View Arches Park' ").toggleClass('slide-in')
+          // }, 3000);
+        },
+        success: function(data, status) {
+          $("h1").html("Success!");
+          var imgList= "";
+          $.each(data.images, function () {
+            imgList += '<img src= "https://peterdoes.it/gnp-tmp/' + this.imgPath + '">';
+          });
+          $('#imgList').append(imgList);
+        },
+        error : function(jqXHR,textStatus,errorThrown) {
+          $("h1").html("Error: " + textStatus + "<br> Response Code: " + jqXHR.status);
+          $("h1").addClass('slide-in');
+        }
+      });
+
+
+    },
+  }; // commands
+  annyang.addCommands(commands);
+  annyang.start();
+}
+
+
